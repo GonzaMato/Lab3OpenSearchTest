e1b3c133dea414dbdc450225fa4dfb61d81463c6
Gonza69420 <gonzalo.mato@ing.austral.edu.ar>
2024-09-21 15:03:59 -0300

    q

---
+++ src/main/kotlin/com/opensearchtest/Lab3OpenSeachTest/Lab3OpenSeachTestApplication.kt
@@ -0,0 +1,11 @@
+package com.opensearchtest.Lab3OpenSeachTest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab3OpenSeachTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<Lab3OpenSeachTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/opensearchtest/Lab3OpenSeachTest/Lab3OpenSeachTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.opensearchtest.Lab3OpenSeachTest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Lab3OpenSeachTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
